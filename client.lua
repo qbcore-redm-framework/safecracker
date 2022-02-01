97c4fcabd5f8f92696670731e8b32ff688ba86fc
LeXarDev | Khalid <kfo@usa.com>
2022-02-02 01:18:47 +0300

    Revert "fix exports Name Qb- to qbr-"
    This reverts commit e68b539913ad43fa0021b57ed1435ed8ff98588f.

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qbr-core']:GetCoreObject()
+local QBCore = exports['qb-core']:GetCoreObject()
 
 function StartMinigame(combo)
 	local Coords = GetEntityCoords(PlayerPedId(), false)
